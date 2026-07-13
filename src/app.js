114507b21ff74f413d855b788d6af800adc2816f
theruderaw <theruderaw678@gmail.com>
2026-07-13 15:46:00 +0530

    added app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import authRoutes from "./features/auth/auth.routes.js";
 import academicRoutes from "./features/academics/academics.routes.js";
 import academicConfigRoutes from "./features/academicConfig/academicConfig.routes.js";
-
+import attendanceRoutes from "./features/attendance/attendance.routes.js"
+import communicationRoutes from "./features/communication/communication.routes.js"
+import dashboardRoutes from "./features/dashboard/dashboard.routes.js"
+import draftsRoutes from "./features/drafts/drafts.routes.js"
 const app = express();
 
 app.use(cors());
@@ -13,6 +16,10 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/academic", academicRoutes);
 app.use("/api/academic-config", academicConfigRoutes);
+app.use("/api/attendance",attendanceRoutes)
+app.use("/api/communication",communicationRoutes)
+app.use("/api/dashboard",dashboardRoutes)
+app.use("/api/drafts",draftsRoutes)
 
 app.get("/", (req, res) => {
     res.json({
--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -3,9 +3,17 @@ import { ZodError } from "zod";
 export const validate = ({ body, params, query }) => {
     return (req, res, next) => {
         try {
-            if (body) req.body = body.parse(req.body);
-            if (params) req.params = params.parse(req.params);
-            if (query) req.query = query.parse(req.query);
+            if (body) {
+                req.body = body.parse(req.body);
+            }
+
+            if (params) {
+                req.params = params.parse(req.params);
+            }
+
+            if (query) {
+                Object.assign(req.query, query.parse(req.query));
+            }
 
             next();
         } catch (err) {
